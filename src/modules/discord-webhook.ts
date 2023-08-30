97fe3980f5c67d21553bb0b46828f195f2a6feb2
hayoung kim <hyoreuno@naver.com>
2023-08-31 01:13:10 +0900

    feat: logger webhook add

--- src/modules/discord-webhook.ts
+++ src/modules/discord-webhook.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-export function sendWebhook(text: string) {
+const LoggerWebHookUrl = `https://discord.com/api/webhooks/1146477027529081003/_6wEnwrlliXelfweQQFKgzuSvP-7KJkLpMSPtMAqN6dlSSncqvZsSWsQSj42xsFB5vPz` as const;
+const ErrorWebHookUrl = `https://discord.com/api/webhooks/1146244914930270208/SNqh8925MJegbvj0OrG04LKcgmKGHALmgcQkFqhpmQW36XVrcK5BGMEFpD3GjaYaANGN`;
+
+export function sendWebhook(text: string, webhookType?: 'Logger') {
   axios.post(
-    `https://discord.com/api/webhooks/1146244914930270208/SNqh8925MJegbvj0OrG04LKcgmKGHALmgcQkFqhpmQW36XVrcK5BGMEFpD3GjaYaANGN`,
+    webhookType === 'Logger' ? LoggerWebHookUrl : ErrorWebHookUrl,
     { content: text },
   );
 }
--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -1,3 +1,6 @@
+import { sendWebhook } from "./discord-webhook";
+
 export function logger(message: string) {
   console.log(`** Message: ${message}`.padEnd(40) + ` ${new Date().toLocaleTimeString()}\n\n`);
+  sendWebhook(message, 'Logger');
 }
